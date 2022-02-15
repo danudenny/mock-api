5464b4905c7baae8ccb0f7c1706711cd54968df1
Denny Danuwijaya <danudenny@gmail.com>
2022-02-15 11:43:20 +0700

    create mock api

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/user-branch', (req, res) => {
+    res.json({
+        'status': 'success',
+        'data': {
+            'firstName': 'Internal',
+            'lastName': 'Internal',
+            'branchIds': [
+                'a3ba1451-8eb0-4740-9e12-43debdc48726'
+            ],
+        }
+    })
+})
+
+app.listen(9000, () => {
+    console.log('App listen in 9000');
+})
